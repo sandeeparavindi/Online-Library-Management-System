76493b4c0b7a1151ac1a33cc92d58dba720a2a25
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-03-08 17:38:59 +0530

    complete AdminDashboard_form.fxml

---
+++ src/main/java/org/example/controller/AdminDashboardFormController.java
@@ -0,0 +1,56 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminDashboardFormController {
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Label txtBookCount;
+
+    @FXML
+    private Label txtBranchCount;
+
+    @FXML
+    private Label txtUserCount;
+
+    @FXML
+    void btnBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHistoryOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHomeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserOnAcrion(ActionEvent event) {
+
+    }
+}
