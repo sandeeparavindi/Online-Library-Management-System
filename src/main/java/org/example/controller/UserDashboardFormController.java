a599b220787da02e3c1c084c49183e3301601ab1
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-03-11 16:03:39 +0530

    Design User DashBoard

--- src/main/java/org/example/controller/UserDashboardFormController.java
+++ src/main/java/org/example/controller/UserDashboardFormController.java
@@ -1,4 +1,56 @@
 package org.example.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class UserDashboardFormController {
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Label txtBookCount;
+
+    @FXML
+    private Label txtBranchCount;
+
+    @FXML
+    private Label txtUserCount;
+
+    @FXML
+    void btnBorrowingOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCredentialOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHomeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnOnAcrion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewOnAction(ActionEvent event) {
+
+    }
 }
